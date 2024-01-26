fb09e401fbcf8c2f1788758c4fdcde55e8e37b12
NikitaKolm <nkolmykov7@gmail.com>
2024-01-26 16:22:05 +0300

    Refactor: 1. updateUser 2. UserController

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -18,34 +18,31 @@ public class UserController {
 
     @GetMapping(value = "/user")
     public String getUserList(ModelMap model) {
-        List<User> users = userService.getAllUsers();
-        User user = new User();
-        model.addAttribute(user);
-        model.addAttribute("userList", users);
+        model.addAttribute(new User());
+        model.addAttribute("userList", userService.getAllUsers());
         return "user";
     }
 
     @PostMapping(value = "/addUser")
-    public String addUser(ModelMap model, @ModelAttribute("user") User user) {
+    public String addUser(@ModelAttribute("user") User user) {
         userService.saveUser(new User(user.getFirstName(), user.getLastName(), user.getEmail()));
         return "redirect:/user";
     }
 
     @PostMapping(value = "/deleteUser")
-    public String removeUserById(ModelMap model, @ModelAttribute("user") User user) {
+    public String removeUserById(@ModelAttribute("user") User user) {
         userService.removeUserById(user.getId());
         return "redirect:/user";
     }
 
     @GetMapping(value = "/updateUser")
     public String getUserUpdateForm(ModelMap model, @RequestParam Long id) {
-        User user = userService.getUserById(id);
-        model.addAttribute("user", user);
+        model.addAttribute("user", userService.getUserById(id));
         return "updateUser";
     }
 
     @PostMapping(value = "/updateUser")
-    public String updateUser(ModelMap model, @ModelAttribute("user") User user) {
+    public String updateUser(@ModelAttribute("user") User user) {
         userService.updateUser(user);
         return "redirect:/user";
     }
